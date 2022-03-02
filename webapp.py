ccae17b16cefbbf02cd29f89e83e6070b1846194
Dant31231 <dcerlez1231@gmail.com>
2022-03-02 09:47:35 -0800

    added tip calc

--- webapp.py
+++ webapp.py
@@ -3,9 +3,20 @@ from flask import Flask, url_for, render_template, request
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 @app.route("/answer")
 def render_answer():
-    bill = request.args['text1']
+    bill = (float(request.args['text1']))/100
+    real_val = request.args['rad1']
+    tip_prsnt = "0"
+    plus_tip = 0.0
+    if real_val == "radio1":
+        plus_tip = bill * 1.15
+        tip_prsnt = "15%"
+    else:
+        plus_tip = bill * 1.20
+        tip_prsnt = "20%"
+
+    ans1 =  str(bill) + " x " + tip_prsnt + " = " + str(plus_tip)
+    return render_template('answer.html', answer1 = ans1)
     
-    return render_template('answer.html', answer1 = bill)
 @app.route("/")
 def render_main():
     return render_template('home.html')
